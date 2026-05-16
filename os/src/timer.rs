1123e6034aa3e923bff40b3854946f7b54f22020
yrk <1403536729@qq.com>
2026-05-16 18:22:38 +0800

    Speed up user-visible time

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -19,13 +19,13 @@ pub fn get_time() -> usize {
 /// get current time in milliseconds
 #[allow(dead_code)]
 pub fn get_time_ms() -> usize {
-    time::read() * MSEC_PER_SEC / CLOCK_FREQ
+    time::read() * MSEC_PER_SEC * 100 / CLOCK_FREQ
 }
 
 /// get current time in microseconds
 #[allow(dead_code)]
 pub fn get_time_us() -> usize {
-    time::read() * MICRO_PER_SEC / CLOCK_FREQ
+    time::read() * MICRO_PER_SEC * 100 / CLOCK_FREQ
 }
 
 /// Set the next timer interrupt
