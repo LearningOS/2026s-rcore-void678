5e25fd612f5180ccca07269ddf2101ad7cce8d2c
yrk <1403536729@qq.com>
2026-05-16 17:50:38 +0800

    Fix spawn fd table initialization

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -294,6 +294,11 @@ impl TaskControlBlock {
                     parent: Some(Arc::downgrade(self)),
                     children: Vec::new(),
                     exit_code: 0,
+                    fd_table: vec![
+                        Some(Arc::new(Stdin)),
+                        Some(Arc::new(Stdout)),
+                        Some(Arc::new(Stdout)),
+                    ],
                     heap_bottom: user_sp,
                     program_brk: user_sp,
                     priority: 16,
